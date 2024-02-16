e91b8a8aa224f372986b750c77fc0b1e5bdc3c66
August Halverson <augusthalverson@gmail.com>
2024-02-16 08:54:36 -0600

    Add example app thats not working

---
+++ example/App.tsx
@@ -0,0 +1,65 @@
+import { StatusBar } from "expo-status-bar";
+import { ColorValue, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { ReorderableFlatList } from "react-native-reorderable-flatlist";
+
+type ExampleData = {
+  id: string;
+  title: string;
+  color: ColorValue;
+};
+
+const exampleData: ExampleData[] = [
+  {
+    id: "1",
+    color: "red",
+    title: "Red",
+  },
+  {
+    id: "2",
+    color: "blue",
+    title: "Blue",
+  },
+  {
+    id: "3",
+    color: "green",
+    title: "Green",
+  },
+  {
+    id: "4",
+    color: "purple",
+    title: "Purple",
+  },
+  {
+    id: "5",
+    color: "orange",
+    title: "Orange",
+  },
+  {
+    id: "6",
+    color: "yellow",
+    title: "Yellow",
+  },
+];
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ReorderableFlatList
+        data={exampleData}
+        renderItem={({ item }) => (
+          <View style={{ backgroundColor: item.color }}>
+            <Text>Item</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+});
